40ea565ab47ff84cd10fe9581515dae2ccc95ccb
Douglas Young <douglasryoung@gmail.com>
2018-05-27 19:28:33 -0700

    Functional Programming Principles in Scala Course 1 Week 2: Practice 2.6 - Operators

--- functional-programming-scala/practice/src/main/scala/course1/week2/Rational.scala
+++ functional-programming-scala/practice/src/main/scala/course1/week2/Rational.scala
@@ -1,16 +1,23 @@
 package course1.week2
 
 class Rational(x: Int, y: Int) {
+  require(y != 0, "denominator must be non-zero")
+
+  def this(x: Int) = this(x, 1)
+
   def numer: Int = x / g
   def denom: Int = y / g
 
+  def +(r: Rational): Rational = add(r)
   def add(r: Rational) = new Rational(
     numer * r.denom + denom * r.numer,
     denom * r.denom
   )
 
+  def unary_- : Rational = neg
   def neg: Rational = new Rational(-numer, denom)
 
+  def -(r:Rational): Rational = sub(r)
   def sub(r: Rational): Rational = add(r.neg)
 
   override def equals(r: Any): Boolean = r match {
--- functional-programming-scala/practice/src/test/scala/course1/week2/RationalSpec.scala
+++ functional-programming-scala/practice/src/test/scala/course1/week2/RationalSpec.scala
@@ -7,8 +7,16 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class RationalSpec extends FunSpec {
   describe("Rational") {
+    it("raises when denominator is zero") {
+      assertThrows[IllegalArgumentException] {
+        new Rational(1, 0)
+      }
+    }
+
     it("returns standard form") {
+      assert(new Rational(1).toString == "1")
       assert(new Rational(1, 2).toString == "1/2")
+      assert(new Rational(2, 1).toString == "2")
     }
 
     it("returns simplified form") {
@@ -19,11 +27,13 @@ class RationalSpec extends FunSpec {
     }
 
     it("returns equality") {
+      assert(new Rational(1, 1) == new Rational(1))
       assert(new Rational(1, 1) == new Rational(1, 1))
       assert(new Rational(1, 1) == new Rational(2, 2))
       assert(new Rational(1, 2) == new Rational(2, 4))
       assert(new Rational(1, 2) == new Rational(10, 20))
       assert(new Rational(1, 2) == new Rational(100, 200))
+      assert(new Rational(2, 1) == new Rational(2))
       assert(new Rational(2, 1) == new Rational(4, 2))
       assert(new Rational(1, 2) != new Rational(1, 3))
       assert(new Rational(2, 1) != new Rational(3, 1))
@@ -41,9 +51,31 @@ class RationalSpec extends FunSpec {
       }
     }
 
+    describe("add") {
+      it("returns the addition of two Rationals") {
+        assert(new Rational(1, 1).add(new Rational(1, 1)) == new Rational(2))
+        assert(new Rational(1, 2).add(new Rational(1, 2)) == new Rational(1))
+        assert(new Rational(1, 2).add(new Rational(1, 4)) == new Rational(3, 4))
+        assert(new Rational(3, 4).add(new Rational(1, 5)) == new Rational(19, 20))
+        assert(new Rational(1, 3).add(new Rational(5, 7)).add(new Rational(3, 2)) == new Rational(107, 42))
+      }
+
+      it("supports the + infix operator on two Rationals") {
+        assert(new Rational(1, 1) + new Rational(1, 1) == new Rational(2))
+        assert(new Rational(1, 2) + new Rational(1, 2) == new Rational(1))
+        assert(new Rational(1, 2) + new Rational(1, 4) == new Rational(3, 4))
+        assert(new Rational(3, 4) + new Rational(1, 5) == new Rational(19, 20))
+        assert(new Rational(1, 3) + new Rational(5, 7) + new Rational(3, 2) == new Rational(107, 42))
+      }
+    }
+
     describe("neg") {
       it("returns the negative of the Rational") {
         assert(new Rational(1, 2).neg == new Rational(-1, 2))
+      }
+
+      it("supports the - prefix operator on one Rational") {
+        assert(-new Rational(1, 2) == new Rational(-1, 2))
       }
     }
 
@@ -54,6 +86,14 @@ class RationalSpec extends FunSpec {
         assert(new Rational(1, 2).sub(new Rational(1, 4)) == new Rational(1, 4))
         assert(new Rational(3, 4).sub(new Rational(1, 5)) == new Rational(11, 20))
         assert(new Rational(1, 3).sub(new Rational(5, 7)).sub(new Rational(3, 2)) == new Rational(-79, 42))
+      }
+
+      it("supports the - infix operator on two Rationals") {
+        assert(new Rational(1, 1) - new Rational(1, 1) == new Rational(0, 1))
+        assert(new Rational(1, 2) - new Rational(1, 2) == new Rational(0, 1))
+        assert(new Rational(1, 2) - new Rational(1, 4) == new Rational(1, 4))
+        assert(new Rational(3, 4) - new Rational(1, 5) == new Rational(11, 20))
+        assert(new Rational(1, 3) - new Rational(5, 7) - new Rational(3, 2) == new Rational(-79, 42))
       }
     }
   }
